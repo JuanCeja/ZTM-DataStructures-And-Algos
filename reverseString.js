baf4b55b61c1493d242e6df4fac73625439713c8
JuanCeja <jmc347ls2@yahoo.com>
2023-09-30 12:24:01 -0700

    create sudo code for our algo

--- reverseString.js
+++ reverseString.js
@@ -3,7 +3,17 @@
 // 'nauJ si eman ym iH'
 
 function reverseString(str) {
+    // make a variable that converts our string to an array
 
+    // create 2 pointers. 1 at the beggining and one at the end
+
+        // create a while loop that stops when beggining pointer > end pointer
+
+        // swap values at the indexes of our pointers
+
+        // move beggining pointer up and end pointer down
+
+    // convert our array to a string and return our string
 }
 
 console.log(reverseString('Hi my name is Juan'));
