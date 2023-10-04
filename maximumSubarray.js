58338e4b6492cb5f30f7cb98f976fc2fdf1f4455
JuanCeja <jmc347ls2@yahoo.com>
2023-10-03 21:15:31 -0700

    create solution in 0(N) time complexity

--- maximumSubarray.js
+++ maximumSubarray.js
@@ -17,8 +17,18 @@
 // Output: 23
 // Explanation: The subarray [5,4,-1,7,8] has the largest sum 23.
 
-const maximumSubarray = () => {
+const maximumSubarray = (arr) => {
+    if(arr.length === 1) return arr[0];
 
+    let maxSum = arr[0];
+    let currentSum = arr[0];
+
+    for(let i = 1; i < arr.length; i++) {
+        currentSum = Math.max(arr[i], arr[i] + currentSum);
+        maxSum = Math.max(currentSum, maxSum);
+    }
+
+    return maxSum;
 }
 
 console.log(maximumSubarray([-2,1,-3,4,-1,2,1,-5,4])); // output: 6
