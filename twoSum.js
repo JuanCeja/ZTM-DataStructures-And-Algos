a0b6b50c395e3c98973d1d21c850773a30353211
JuanCeja <jmc347ls2@yahoo.com>
2023-10-03 18:40:58 -0700

    create pseudo code solution

--- twoSum.js
+++ twoSum.js
@@ -17,24 +17,41 @@
 // Input: nums = [3,3], target = 6
 // Output: [0,1]
 
-function twoSum(nums, target) {
-    // Iterate through the array
-    for (let i = 0; i < nums.length; i++) {
 
-        // For each element, iterate through the rest of the array
-        for (let j = i + 1; j < nums.length; j++) {
+// ==== solution with O(n^2) time complexity ====
+// function twoSum(nums, target) {
+//     // Iterate through the array
+//     for (let i = 0; i < nums.length; i++) {
 
-            // Check if the current pair of elements adds up to the target
-            if (nums[i] + nums[j] === target) {
+//         // For each element, iterate through the rest of the array
+//         for (let j = i + 1; j < nums.length; j++) {
 
-                // Return the indices of the two numbers
-                return [i, j];
-            }
-        }
+//             // Check if the current pair of elements adds up to the target
+//             if (nums[i] + nums[j] === target) {
+
+//                 // Return the indices of the two numbers
+//                 return [i, j];
+//             }
+//         }
+//     }
+//     // If no solution is found, return an empty array or handle the error as needed
+//     return [];
+// }
+
+// ==== solution with O(n) time complexity ====
+    function twoSum (arr, target) {
+        // create a map to store the pair for target 
+        // create a target pair number
+
+        // use a loop to iterate the array
+
+            // IF the value is not in the map store it with its indecy as the value
+
+            // check to see if the map has the pair value to make the target
+                // if so return the indices
+
+        // return empty array if pair does not exist
     }
-    // If no solution is found, return an empty array or handle the error as needed
-    return [];
-}
 
 console.log(twoSum([2, 7, 11, 15], 9)); // output: [0, 1]
 console.log(twoSum([3, 2, 4], 6)); // output: [1, 2]
