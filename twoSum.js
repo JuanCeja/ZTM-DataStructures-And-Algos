e376b86f666bbc112150c2f7550eb852441dd2ba
JuanCeja <jmc347ls2@yahoo.com>
2023-10-03 18:33:27 -0700

    create solution with O(n^2) time complexity

--- twoSum.js
+++ twoSum.js
@@ -17,10 +17,25 @@
 // Input: nums = [3,3], target = 6
 // Output: [0,1]
 
-function twoSum(arr, target) {
+function twoSum(nums, target) {
+    // Iterate through the array
+    for (let i = 0; i < nums.length; i++) {
 
+        // For each element, iterate through the rest of the array
+        for (let j = i + 1; j < nums.length; j++) {
+
+            // Check if the current pair of elements adds up to the target
+            if (nums[i] + nums[j] === target) {
+
+                // Return the indices of the two numbers
+                return [i, j];
+            }
+        }
+    }
+    // If no solution is found, return an empty array or handle the error as needed
+    return [];
 }
 
-console.log(twoSum([2,7,11,15], 9)); // output: 
-console.log(twoSum([3,2,4], 6)); // output: 
-console.log(twoSum([3,3], 6)); // output: 
+console.log(twoSum([2, 7, 11, 15], 9)); // output: [0, 1]
+console.log(twoSum([3, 2, 4], 6)); // output: [1, 2]
+console.log(twoSum([3, 3], 6)); // output: [0, 1]
