8f95c81c70a17492670b66658c3d7932f9c04bec
JuanCeja <jmc347ls2@yahoo.com>
2023-10-04 19:51:59 -0700

    create solution with O(n) time complexity

--- moveZeroes.js
+++ moveZeroes.js
@@ -14,16 +14,25 @@
 
 const moveZeroes = (nums) => {
     // create var to store index to make a swap to
+    let index = 0;
 
     // iterate the nums array
-
+    for (let i = 0; i < nums.length; i++) {
         // IF the nums array value != 0 swap it with the index then increase index
+        if(nums[i] !== 0) {
+            [nums[i], nums[index]] = [nums[index], nums[i]];
+            index++;
+        }
+    }
 
     // use second loop to iterate the array starting from the index
-
+    for(let j = index; j < nums.length; j++) {
         // replace every value with a 0
+        nums[j] = 0;
+    }
 
     // return our nums array
+    return nums;
 };
 
 console.log(moveZeroes([0,1,0,3,12])) // output: [1,3,12,0,0]
