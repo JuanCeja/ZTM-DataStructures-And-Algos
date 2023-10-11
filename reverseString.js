fd0bebb07f3feff76337a0ea4d94e1f343b5c9a3
JuanCeja <jmc347ls2@yahoo.com>
2023-10-10 19:51:37 -0700

    initial commit

--- reverseString.js
+++
@@ -1,9 +0,0 @@
-// Create a function that reverses a string:
-// 'Hi my name is Juan' should be: 
-// 'nauJ si eman ym iH'
-
-function reverseString(str) {
-
-}
-
-console.log(reverseString('Hi my name is Juan'));
