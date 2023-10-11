5c431829cd9651947e2476ad72652ab60ca2b15c
JuanCeja <jmc347ls2@yahoo.com>
2023-10-10 21:33:41 -0700

    create print list method

--- linked-list.js
+++ linked-list.js
@@ -30,10 +30,25 @@ class LinkedList {
         this.length++;
         return this;
     }
+
+    printList() {
+        const array = [];
+        let current = this.head;
+        while(current !== null) {
+            array.push(current.value);
+            current = current.next;
+        }
+        return array;
+    }
+
+    insert(index, value) {
+
+    }
 }
 
 let myLinkedList = new LinkedList(10);
 myLinkedList.append(5);
 myLinkedList.append(16);
 myLinkedList.prepend(1);
-console.log(myLinkedList);
+myLinkedList.insert(2, 99);
+console.log(myLinkedList.printList());
