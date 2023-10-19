ff0cd652411d702e9c8ba20d67ab5ff2156caeee
JuanCeja <jmc347ls2@yahoo.com>
2023-10-18 20:35:39 -0700

    create solutions for all methods for queue using Linked list

--- queue-linkedList.js
+++ queue-linkedList.js
@@ -13,18 +13,48 @@ class Queue {
     }
 
     peek() {
-
+        return this.first;
     }
 
     enqueue(value) {
+        const newNode = new Node(value);
 
+        if (this.length === 0) {
+            this.first = newNode;
+            this.last = newNode;
+        } else {
+            this.last.next = newNode;
+            this.last = newNode;
+        }
+        this.length++;
+        return this;
     }
 
     dequeue() {
-
+        if(!this.first) return null;
+        if (this.length === 1) {
+            this.first = null;
+            this.last = null;
+            this.length--;
+            return this;
+        }
+        let removedNode = this.first;
+        this.first = this.first.next;
+        removedNode.next = null;
+        this.length--;
+        return removedNode;
     }
 
     isEmpty() {
-
+        return this.length === 0 ? true : false;
     }
 }
+
+let myQueue = new Queue();
+myQueue.enqueue('Joy');
+myQueue.enqueue('Matt');
+myQueue.enqueue('Pavel');
+myQueue.enqueue('Samir');
+console.log('removed', myQueue.dequeue());
+console.log(myQueue.peek());
+console.log(myQueue);
