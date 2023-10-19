5e7e42cf78d909bafd8b99b5159622435192beaa
JuanCeja <jmc347ls2@yahoo.com>
2023-10-18 18:55:31 -0700

    create solutions for stack array methods

--- stack-array.js
+++ stack-array.js
@@ -4,18 +4,29 @@ class Stack {
     }
 
     peek() {
-        
+        return this.array[this.array.length - 1];
     }
 
     push(value) {
-
+        this.array.push(value);
+        return this;
     }
 
     pop() {
-
+        let removed = this.array.pop();
+        return removed;
     }
 
     isEmpty() {
-
+        return this.array.length === 0 ? true : false;
     }
 }
+
+let myStack = new Stack();
+myStack.push('Google');
+myStack.push('Udemy');
+myStack.push('Discord');
+console.log(myStack.pop());
+console.log(myStack.isEmpty());
+console.log('peek', myStack.peek());
+console.log(myStack);
