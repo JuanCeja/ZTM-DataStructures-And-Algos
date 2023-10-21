99622bbd5bf42de267b5031674a8b68e1dfceb29
JuanCeja <jmc347ls2@yahoo.com>
2023-10-20 19:56:06 -0700

    create insert method for binary search tree

--- binary-search-tree.js
+++ binary-search-tree.js
@@ -12,7 +12,32 @@ class BinarySearchTree {
     }
 
     insert(value) {
+        const newNode = new Node(value);
 
+        if (!this.root) {
+            this.root = newNode;
+        }
+
+        let current = this.root;
+        while(current) {
+            if(current.value === newNode.value) {
+                return "Node already exists";
+            }
+            if(newNode.value < current.value) {
+                if(!current.left) {
+                    current.left = newNode;
+                    return this;
+                }
+                current = current.left;
+            } else {
+                if(!current.right) {
+                    current.right = newNode;
+                    return this;
+                }
+                current = current.right;
+            }
+        }
+        return this;
     }
 
     lookup(value) {
@@ -25,3 +50,11 @@ class BinarySearchTree {
 }
 
 const tree = new BinarySearchTree();
+tree.insert(7);
+tree.insert(9);
+tree.insert(3);
+tree.insert(6);
+tree.insert(2);
+tree.insert(8);
+tree.insert(11);
+console.log(tree);
