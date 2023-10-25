08d6bf311dcc905117034a7b7c3071e8c72b9937
JuanCeja <jmc347ls2@yahoo.com>
2023-10-24 19:58:55 -0700

    create graph file

---
+++ graph.js
@@ -0,0 +1,16 @@
+// A graph data structure is a mathematical and computational representation of a set of objects, called nodes (or vertices), connected by a collection of links (or edges). Graphs are used to model and analyze relationships between various entities or elements. They are composed of two fundamental components:
+
+// 1. Nodes (Vertices): Nodes represent the entities or elements within the graph. Each node can store data associated with the entity it represents.
+
+// 2. Edges (Links): Edges define the connections or relationships between nodes. They can be directional (directed) or undirectional (undirected) and may or may not have associated weights or values.
+
+// Graphs are versatile and find applications in various domains, including computer science, mathematics, social networks, transportation networks, and more. They come in different types, such as directed graphs (digraphs), undirected graphs, weighted graphs, and more, each serving specific purposes in modeling and solving real-world problems.
+
+// ==== GRAPH ====
+//      2 ------- 0
+//     / \
+//    /   \
+//   1 --- 3
+
+// Edge List
+const graph = [[0, 2], [2, 3], [2, 1], [1, 3]];
