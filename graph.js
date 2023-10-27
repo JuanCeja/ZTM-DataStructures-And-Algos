928541c38b585e6cdc91b147bee6d62123b35e34
JuanCeja <jmc347ls2@yahoo.com>
2023-10-26 20:42:21 -0700

    create solution for showConnections method for Graph

--- graph.js
+++ graph.js
@@ -41,7 +41,16 @@ class Graph {
     }
 
     showConnections() {
-        
+        const allNodes = Object.keys(this.adjacencyList);
+        for (let node of allNodes) {
+            let nodeConnections = this.adjacencyList[node];
+            let connections = '';
+            let vertex;
+            for (vertex of nodeConnections) {
+                connections += vertex + " ";
+            }
+            console.log(node + "--->" + connections);
+        }
     }
 }
 
@@ -53,13 +62,13 @@ myGraph.addVertex('3');
 myGraph.addVertex('4');
 myGraph.addVertex('5');
 myGraph.addVertex('6');
-myGraph.addEdge('3','1');
-myGraph.addEdge('3','4');
-myGraph.addEdge('4','2');
-myGraph.addEdge('4','5');
-myGraph.addEdge('1','2');
-myGraph.addEdge('1','0');
-myGraph.addEdge('0','2');
-myGraph.addEdge('6','5');
+myGraph.addEdge('3', '1');
+myGraph.addEdge('3', '4');
+myGraph.addEdge('4', '2');
+myGraph.addEdge('4', '5');
+myGraph.addEdge('1', '2');
+myGraph.addEdge('1', '0');
+myGraph.addEdge('0', '2');
+myGraph.addEdge('6', '5');
 
 myGraph.showConnections();
