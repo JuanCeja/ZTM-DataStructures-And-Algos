82394f80dd3d6b72f8bc37bf4166de1ccfaed7e0
JuanCeja <jmc347ls2@yahoo.com>
2023-10-27 21:09:23 -0700

    create solution for findFactorialRecursively method

--- recursion.js
+++ recursion.js
@@ -1,7 +1,8 @@
 // Recursion is a programming and mathematical concept where a function or a process calls itself in order to solve a problem by breaking it down into smaller, more manageable sub-problems. This technique is particularly useful for solving complex problems that can be expressed in terms of simpler, similar instances of the same problem. Recursion typically involves a base case, which defines the simplest scenario that can be solved directly, and a recursive case, where the problem is reduced in size and solved by applying the same function to the smaller sub-problems. Recursion is widely used in computer science, enabling elegant and concise solutions to a variety of problems, such as tree traversal, factorial calculation, and various sorting algorithms like quicksort and merge sort.
 
 function findFactorialRecursive(number) {
-    
+    if(number === 1) return 1;
+    return number * findFactorialRecursive(number - 1)
 };
 
 function findFactorialIteratively(number) {
@@ -12,5 +13,5 @@ function findFactorialIteratively(number) {
     return sum;
 };
 
-// console.log(findFactorialRecursive(5));
-console.log(findFactorialIteratively(5));
+console.log(findFactorialRecursive(5));
+// console.log(findFactorialIteratively(5));
