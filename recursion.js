5a000b89863c32eda5a95cd8edd9a4e89845be86
JuanCeja <jmc347ls2@yahoo.com>
2023-10-27 21:26:27 -0700

    create methods for fibunacci

--- recursion.js
+++ recursion.js
@@ -14,4 +14,20 @@ function findFactorialIteratively(number) {
 };
 
 console.log(findFactorialRecursive(5));
-// console.log(findFactorialIteratively(5));
+console.log(findFactorialIteratively(5));
+
+// Given a number N return the index value of the Fibonacci sequence, where the sequence is:
+
+// 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144 ...
+// the pattern of the sequence is that each value is the sum of the 2 previous values, that means that for N=5 -> 2 + 3
+
+function fibonacciIterative(n) {
+
+}
+
+function fibonacciRecursively(n) {
+
+}
+
+console.log(fibonacciIterative(5));
+console.log(fibonacciRecursively(5));
