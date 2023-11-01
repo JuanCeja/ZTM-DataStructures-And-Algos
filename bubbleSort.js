8ce82d245d95335ec265f11aebc95bee8f9926bb
JuanCeja <jmc347ls2@yahoo.com>
2023-10-31 21:06:30 -0700

    write brief description of what bubble sort is

---
+++ bubbleSort.js
@@ -0,0 +1,7 @@
+// Bubble sort is a simple sorting algorithm used to arrange elements in a list or array. It works by repeatedly stepping through the list, comparing adjacent elements, and swapping them if they are in the wrong order. The pass through the list is repeated until no swaps are needed, indicating that the list is sorted. It is called "bubble sort" because the smaller elements "bubble" to the top of the list as the algorithm progresses. While bubble sort is straightforward, it is not very efficient and is primarily used for educational purposes or on small datasets due to its O(n^2) time complexity, where n is the number of elements to be sorted.
+
+const bubbleSort = (arr) => {
+
+};
+
+console.log(bubbleSort([4, 8, 2, 7, 6, 0, 1, 3, 9]));
