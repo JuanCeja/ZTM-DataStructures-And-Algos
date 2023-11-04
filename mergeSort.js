62be99c488db531cf5a02fb5205a80d964b2dedb
JuanCeja <jmc347ls2@yahoo.com>
2023-11-03 20:53:58 -0700

    initial commit for merge sort

---
+++ mergeSort.js
@@ -0,0 +1,13 @@
+// Merge sort is a popular and efficient sorting algorithm that uses a divide-and-conquer strategy to sort an array or list of elements. The algorithm works by recursively dividing the input into smaller sub-arrays until each sub-array contains only one element (making them inherently sorted). It then merges these smaller sub-arrays back together in a sorted order to produce a fully sorted array. The key idea is to repeatedly combine two sorted sub-arrays into a larger sorted sub-array until the entire array is sorted. Merge sort is known for its stability and consistent performance, making it a reliable choice for sorting large datasets.
+
+const mergeSort = (arr) => {
+    if (arr.length === 1) return arr;
+
+    return merge(mergeSort(left), mergeSort(right));
+};
+
+const merge = (left, right) => {
+    //
+};
+
+console.log(mergeSort([17, 22, 21, 10, 399, 464, 6, 711, 8, 0, 16]))
