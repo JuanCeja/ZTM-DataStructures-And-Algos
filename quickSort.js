b72652adaac4213365b687d93ae6086603aa1cca
JuanCeja <jmc347ls2@yahoo.com>
2023-11-04 12:31:40 -0700

    initial commit for quickSort

---
+++ quickSort.js
@@ -0,0 +1,7 @@
+// Quick Sort is a fast and efficient sorting algorithm that uses a divide-and-conquer approach. It works by selecting a "pivot" element from the array and rearranges the elements so that all items smaller than the pivot are on the left, and all items larger than the pivot are on the right. This process is applied recursively to the sub-arrays on both sides of the pivot until the entire array is sorted. Quick Sort has an average time complexity of O(n log n) and is widely used for sorting large datasets.
+
+const quickSort = (arr) => {
+    
+};
+
+console.log(quickSort([17, 22, 21, 10, 399, 464, 6, 711, 8, 0, 16]));
