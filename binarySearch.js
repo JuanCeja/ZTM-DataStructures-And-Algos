9523ff604b282274b46af43020c47ce93eb26297
JuanCeja <jmc347ls2@yahoo.com>
2023-11-04 19:39:12 -0700

    initial commit for binary search

---
+++ binarySearch.js
@@ -0,0 +1,7 @@
+// Binary search is an efficient algorithm for finding a specific element in a sorted list or array by repeatedly dividing the search space in half, reducing the number of possibilities with each comparison until the target is found or determined to be absent.
+
+const binarySearch = (arr) => {
+
+};
+
+console.log(binarySearch([0, 2, 4, 6, 7, 8, 9, 34, 678, 899, 1000]));
