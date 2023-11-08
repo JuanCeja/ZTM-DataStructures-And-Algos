b0ce5e401a98d8f0ced620077a167dad28183365
JuanCeja <jmc347ls2@yahoo.com>
2023-11-07 16:43:36 -0800

    create basic DFS & BFS questionaire

---
+++ DFS-BFS-generalQuestions.js
@@ -0,0 +1,12 @@
+// If you know a solution is not far from the root of the tree: 
+BFS
+// If the tree is very deep and solutions are rare: 
+DFS --- BFS (DFS will take too long)
+// if the tree is very wide: 
+BFS --- DFS (BFS will need too much memory)
+// if solutions are frequent but located deep in the tree: 
+DFS
+// determining wheter a path exists between two nodes: 
+DFS
+// finding the shortest path: 
+BFS
