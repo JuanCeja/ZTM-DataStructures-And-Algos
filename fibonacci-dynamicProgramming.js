146042b4fdc408eaee7f68ace612932f3456c60d
JuanCeja <jmc347ls2@yahoo.com>
2023-11-14 19:05:04 -0800

    create solution for fibonacci

--- fibonacci-dynamicProgramming.js
+++ fibonacci-dynamicProgramming.js
@@ -1,7 +1,8 @@
 // The Fibonacci sequence is a series of numbers where each number is the sum of the two preceding ones. In dynamic programming, the algorithm is optimized using memoization. This involves storing previously calculated Fibonacci numbers to avoid redundant computations. By checking the cache before calculations, the algorithm improves efficiency, particularly for larger indices.
 
-const fibonacciDynamicProgramming = (n) => {
-    
+const fibonacci = (n) => {
+    if (n < 2) return n;
+    return fibonacci(n - 1) + fibonacci(n - 2);
 };
 
-console.log(fibonacciDynamicProgramming(7));
+console.log(fibonacci(6));
