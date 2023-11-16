90bdd5651947831e5a67559e06e151bd05fbc3be
JuanCeja <jmc347ls2@yahoo.com>
2023-11-15 18:54:59 -0800

    create solution for houseRobber algo

--- houseRobber.js
+++ houseRobber.js
@@ -16,8 +16,19 @@
 // Total amount you can rob = 2 + 9 + 1 = 12.
 
 const rob = (nums) => {
+    if (!nums.length) return 0;
+    if (nums.length === 1) return nums[0];
+    if (nums.length === 2) return Math.max(nums[0], nums[1]);
 
+    let dp = new Array(nums.length);
+    dp[0] = nums[0];
+    dp[1] = Math.max(nums[0], nums[1]);
+    for (let i = 2; i < nums.length; i++) {
+        dp[i] = Math.max(dp[i - 2] + nums[i], dp[i - 1]);
+    };
+    return dp[nums.length - 1];
 };
 
-console.log(rob([1,2,3,1])); // 4
-console.log(rob([2,7,9,3,1])); // 12
+console.log(rob([1, 2, 3, 1])); // 4
+console.log(rob([2, 7, 9, 3, 1])); // 12
+console.log(rob([2, 7, 3, 1, 4, 2, 1, 8])); // 19
