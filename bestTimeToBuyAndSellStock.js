d2465787e5f283a96c6f6e735d133918a3e269bf
JuanCeja <jmc347ls2@yahoo.com>
2023-11-17 19:23:54 -0800

    initial commit for climb stairs algorithm

--- bestTimeToBuyAndSellStock.js
+++ bestTimeToBuyAndSellStock.js
@@ -16,26 +16,21 @@
 // Explanation: In this case, no transactions are done and the max profit = 0.
 
 const maxProfit = (prices) => {
-    // at the beginning the minimum price is the first price
-    // at the beginning the minimum profit is 0
     let min = prices[0];
     let profit = 0;
 
-    // iterate the prices starting at 1
     for (let i = 1; i < prices.length; i++) {
-        // if the current price is less update the buying price
         if (prices[i] < min) {
             min = prices[i]
         } else {
-            // else check if we can get a better profit
             if (prices[i] - min > profit) {
                 profit = prices[i] - min;
             }
         }
     }
-    // return our profit
     return profit;
 };
 
 console.log(maxProfit([7, 1, 5, 3, 6, 4])); // 5
 console.log(maxProfit([7, 6, 4, 3, 1])); // 0
+console.log(maxProfit([7, 2, 4, 5, 11, 3, 46, 4])); // 44
