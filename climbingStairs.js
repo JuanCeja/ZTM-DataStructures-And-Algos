53a33d21aad16eaac555f9a25dcd474bdc3b6abf
JuanCeja <jmc347ls2@yahoo.com>
2023-11-18 18:49:40 -0800

    initial commit for climbing stairs algorithm

--- climbingStairs.js
+++ climbingStairs.js
@@ -18,14 +18,7 @@
 // 3. 2 steps + 1 step
 
 const climbStairs = (n) => {
-    if (n === 1) return 1;
-    let dp = new Array(n + 1);
-    dp[1] = 1;
-    dp[2] = 2;
-    for (let i = 3; i <= n; i++) {
-        dp[i] = dp[i - 1] + dp[i - 2];
-    }
-    return dp[n];
+    
 };
 
 console.log(climbStairs(2)); // 2
