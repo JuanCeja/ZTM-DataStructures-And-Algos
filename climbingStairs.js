eef20957e6e350a0e5e6e5422ea079bc3c131858
JuanCeja <jmc347ls2@yahoo.com>
2023-11-18 18:53:27 -0800

    pseudo code for climbing stairs

--- climbingStairs.js
+++ climbingStairs.js
@@ -18,9 +18,14 @@
 // 3. 2 steps + 1 step
 
 const climbStairs = (n) => {
-    
+    // create an array to store our combinations
+    // we initialize index 1 to 1 and index 2 to 2 because the amount of combinations that can be created at those steps
+    // we create a loop to go up to n 
+        // the current step is always going to have the total combinations of its previous 2 steps. so we can add it
+    // we return the last index in the array
 };
 
 console.log(climbStairs(2)); // 2
 console.log(climbStairs(3)); // 3
 console.log(climbStairs(4)); // 5
+console.log(climbStairs(7)); // 5
